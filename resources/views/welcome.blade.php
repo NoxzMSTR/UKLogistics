b4c23c5b09f95603bb8a4ac6839807cead0b76bf
Ammad Javed <42188571+NoxzMSTR@users.noreply.github.com>
2022-08-25 18:19:49 +0500

    Checking

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -212,7 +212,7 @@
         <nav class="navbar navbar-expand-lg">
             <div class="container">
                 <a class="navbar-brand" href="index.html">
-                    <h2>Stand Blog<em>.</em></h2>
+                    <h2>UK Logistics<em>.</em></h2>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
                     aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
